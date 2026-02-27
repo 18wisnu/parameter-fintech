b117226030f9ba3510880cac28025769359d70f0
18wisnu <148938542+18wisnu@users.noreply.github.com>
2026-02-27 16:47:29 +0700

    Fix HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Tambahkan baris ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan 3 baris ini: Jika aplikasi diakses selain dari localhost, paksa pakai HTTPS
+        if (config('app.env') === 'production' || request()->header('X-Forwarded-Proto') == 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
